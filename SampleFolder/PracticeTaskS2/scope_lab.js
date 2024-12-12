034f6ff94152b8145813523dc9aca8d0ef183d06
Silver4567J <silgold54@gmail.com>
2024-12-12 14:58:10 -0500

    Ejercicio2

---
+++ SampleFolder/PracticeTaskS2/scope_lab.js
@@ -0,0 +1,19 @@
+function practiceTaskS2() {
+    let funcionLet = "Soy una variable de tipo let";
+    const funcionConst = "Soy una varialbe de tipo constante y soy el numero 20";
+    var funcionVar = "Soy una varible de tipo var";
+
+    // funcionLet = "cambie de apodo";
+    // funcionConst = 20;
+    // funcionVar = "cambio de var";
+
+}
+funcionLet = "cambie de apodo";
+funcionConst = 20;
+funcionVar = "cambio de var";
+practiceTaskS2();
+
+
+console.log(funcionLet);
+console.log(funcionConst);
+console.log(funcionVar);
